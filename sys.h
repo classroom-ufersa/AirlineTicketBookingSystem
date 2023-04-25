43a545df1379a7337b5d26bf553f987fbab36680
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-04-25 16:45:10 +0000

    sys.h update

--- sys.h
+++ sys.h
@@ -1,1 +1,20 @@
-// Tad
+// Makes an airline reservation
+void book();
+
+// Deletes a reservation
+void delete_book();
+
+// Lists all reservations
+void list_book();
+
+// Searches all reservations
+void search_book();
+
+// Verifies if the destination is available
+void aviability();
+
+// Verifies how many passangers are on the flight
+void passangers();
+
+// Leaves the menu and finishes the program
+void leave();
