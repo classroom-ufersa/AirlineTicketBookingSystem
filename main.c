5cfad84f7bbb51bc941311cf1eb3cbd387775e6f
João Pedro <116277457+Joao11110@users.noreply.github.com>
2023-05-16 17:34:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,55 @@
 #include<stdio.h>
-#include"airplane.h"
-#include"ticket.h"
+#include<stdlib.h>
+#include"passagens.c"
+#include"aviao.c"
 
-int main(){
-    int op;
-
-    do
-    {
-    printf("\n\n--Menu--\n\n");
-    printf("Make a booking\n");
-    printf("Delete a booking\n");
-    printf("List bookings\n");
-    printf("Search bookings\n");
-    printf("Destination aviability\n");
-    printf("Your flight capacity\n");
-    printf("Leave menu\n");
-    switch (op)
-    {
-    case 1:
-        
-        break;
-    
-    default:
-        break;
-    }
-
-
-
-
-
-
-
-    } while (op != );
-
-
-
-    return 0;
+int main(void){
+    Aviao *aviao = NULL; // Variável struct passagem que manipula dados sobre voos
+    Passagem *p = NULL; // Variável struct passagem que manipula dados dos passageiros
+    FILE *fp = NULL; // Variável para manipulação de arquivos
+    int opcao; // Variável para escolher uma opção no menu
+    // Inicio do laço de repetição
+    do {
+        printf("\n|---------------------------------|\n");
+        printf("|     MENU - PASSAGENS AEREAS     |\n");
+        printf("|---------------------------------|\n");
+        printf("| 1 - Inserir passagem            |\n");
+        printf("| 2 - Remover passagem            |\n");
+        printf("| 3 - Listar passagens            |\n");
+        printf("| 4 - Buscar passagens            |\n");
+        printf("| 5 - Consultar vagas disponiveis |\n");
+        printf("| 6 - Quantitativo de passageiros |\n");
+        printf("| 7 - Sair do programa            |\n");
+        printf("|---------------------------------|\n\n");
+        opcao = apenas_numeros();
+        switch(opcao) {
+            case 1:
+                insere_passagem(&p);
+                break;
+            case 2:
+                remove_passagem(&p);
+                break;
+            case 3:
+                lista_passagens(p, fp);
+                break;
+            case 4:
+                busca_passagem(p);
+                break;
+            case 5:
+                // consultar_vagas(aviao);
+                break;
+            case 6:
+                quantidade_passageiros(p);
+                break;
+            case 7:
+                // libera_lista(p);
+                printf("\nObrigado por usar o nosso programa!\n\n");
+                exit(1);
+                break;
+            default:
+                printf("Opcao invalida! Tente novomente.");
+                break;
+        }
+    } while (opcao != 7); // Fim do laço de repetição
+    return 0; // Fim do programa
 }
